1852251ae9950aedb4e3e17d5f128e772bf27671
root <root@localhost.localdomain>
2018-06-10 16:57:30 +0300

    Latest

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,14 +11,14 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity
 {
-    int number = 0;
-    int quantity = 10;
+    int quantity = 97;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,33 +43,69 @@ public class MainActivity extends AppCompatActivity
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         String quantitu = quantityTextView.getText().toString();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasCream,hasChocolate);
+
         String priceMessage = "Name "+nameTextt;
         priceMessage = priceMessage + "\nAdd Whipped Cream? " + hasCream;
         priceMessage = priceMessage + "\nAdd Chocolate? " + hasChocolate;
         priceMessage = priceMessage + "\nQuantity " + quantitu;
         priceMessage = priceMessage + "\nTotal is Ksh. "+ price;
         priceMessage = priceMessage + "\nThank you";
-        displayMessage(priceMessage);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Intent");
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 
     /**
      * Calculates the price of the order.
      *
      */
-    private int calculatePrice() {
-        return  quantity * 5;
+    private int calculatePrice(boolean cream, boolean choco) {
+        int basePrice = 5;
+
+        if (cream){
+            basePrice = basePrice + 1;
+        }
+
+        if (choco){
+            basePrice = basePrice + 2;
+        }
+
+        return quantity * basePrice;
     }
     public void increment(View view)
     {
-        number += 1;
-        display(number);
+        if (quantity < 100){
+            quantity += 1;
+            display(quantity);
+        }
+        else{
+            Toast.makeText(this,"Cannot order more than 100 cups",Toast.LENGTH_SHORT).show();
+            display(quantity);
+
+
+        }
+
     }
 
     public void decrement(View view)
     {
-        number -= 1;
-        display(number);
+        if(quantity > 1){
+            quantity -= 1;
+            display(quantity);
+        }
+        else {
+            Toast.makeText(this,"Cannot order less than 1 cup",Toast.LENGTH_SHORT).show();
+            display(quantity);
+            display(quantity);
+        }
     }
 
     /**
@@ -79,11 +117,6 @@ public class MainActivity extends AppCompatActivity
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number)
-    {
-        TextView quantityTextView = (TextView) findViewById(R.id.order_summury_text_view);
-        quantityTextView.setText("" + number);
-    }
     /**
      * This method displays the given text on the screen.
      */
